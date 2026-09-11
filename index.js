b2d25f30b9980b6a912214b735124052681ce9dc
ash14feb <ashwaq.cs@gmail.com>
2026-09-11 11:57:59 +0530

    updated

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const problemsRoutes = require('./api/problems');
 const reportsRoutes = require('./api/reports');
 const mosquesRoutes = require('./api/mosques');
 const arcadeRoutes = require('./api/arcadepayment');
+const targetsRoutes = require('./api/targets');
 const app = express();
 
 // Middleware
@@ -43,6 +44,7 @@ app.use('/api/problems', problemsRoutes);
 app.use('/api/reports', reportsRoutes);
 app.use('/api/mosques', mosquesRoutes);
 app.use('/api/arcadepayment', arcadeRoutes);
+app.use('/api/targets', targetsRoutes);
 
 // 404 handler
 app.use('*', (req, res) => {
